a258703b1b83040dd5fb4facf5e0f8cb4e3866c1
OnGarces <>
2018-06-12 18:00:54 -0500

    upload-project

---
+++ src/main/java/co/com/poli/AppSubastas/AppSubastasApplication.java
@@ -0,0 +1,12 @@
+package co.com.poli.AppSubastas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSubastasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSubastasApplication.class, args);
+	}
+}
